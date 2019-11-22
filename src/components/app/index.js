3d8d1a5805dfbf917d05d21baa895d5b31c928d9
Naveen Vignesh <naveenvignesh5.nv@gmail.com>
2019-11-22 19:45:05 +0530

    Basic setup

---
+++ src/components/app/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './app.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
