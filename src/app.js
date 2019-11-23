32ef4898250454ab12a2b80ed4e236e11f1ce4ac
Naveen Vignesh <naveenvignesh5.nv@gmail.com>
2019-11-23 17:33:07 +0530

    Updates index and adds network hoc component

--- src/app.js
+++ src/app.js
@@ -3,9 +3,14 @@ import { Provider } from "react-redux";
 
 import Store from './redux/store';
 
+import withNetwork from './libs/withNetwork';
+
 // home page
 import Home from "./pages/home";
 import axios from "axios";
+
+// styles
+import './styles/app.sass';
 
 global.axios = axios.create({
     baseURL: '//api.giphy.com/v1',
@@ -19,4 +24,5 @@ const App = () => {
   );
 };
 
-export default App;
+export default withNetwork(App);
+
