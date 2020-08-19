8c2a43105afe242fc07774537706d68b69239629
Naveen Vignesh <naveenvignesh5.nv@gmail.com>
2020-08-19 11:57:30 +0530

    Updates

---
+++ precache-manifest.0d63d741b558a3ba999d2f44f4bcadee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7feaed73e0a89da5f8846acd55a9a8d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5b6552020568a8e57b6",
+    "url": "/static/css/main.a69a18bf.chunk.css"
+  },
+  {
+    "revision": "e9d063ce1a35bf6ad482",
+    "url": "/static/js/2.8f7b8e7b.chunk.js"
+  },
+  {
+    "revision": "b5b6552020568a8e57b6",
+    "url": "/static/js/main.1499b053.chunk.js"
+  },
+  {
+    "revision": "105cf76cb06fc09482b6",
+    "url": "/static/js/runtime-main.2374a3a6.js"
+  }
+]);
